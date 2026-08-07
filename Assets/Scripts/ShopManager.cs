1087e45d16f71f76d78f21a6d055fd05a83c6bd2
Andrew T Han <taeil564@gmail.com>
2026-08-07 18:06:31 -0400

    ShopManager SetUp but waiting till full picture

--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -2,15 +2,38 @@ using UnityEngine;
 
 public class ShopManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private bool isShopOpen;
+
+    private bool[] shellUnlocked = new bool[3]; //Increase to 5 for larger scope
+    private int[] shellCosts = new int[3];
+    private double fund = 0;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (shellCosts[1] > fund) 
+        { 
+            //Grey-out button
+        }
+        if (shellCosts[2] > fund)
+        {
+            //Grey-out button
+        }
+    }
+
+    public void buyShell2()
+    {
+        if (shellCosts[1] > fund) 
+        { 
+            //Unlock Shell Lvl2
+        }
+    }
+
+    public void buyShell3()
+    {
+        if (shellCosts[2] > fund)
+        {
+            //Unlock Shell Lvl3
+        }
     }
 }
