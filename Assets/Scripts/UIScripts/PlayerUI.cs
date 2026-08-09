b18c48b6523d59dd4282ee8b1a0e51fd28e0ff60
Andrew T Han <taeil564@gmail.com>
2026-08-09 00:06:22 -0400

    More PlayerUI and ShopFixes

--- Assets/Scripts/UIScripts/PlayerUI.cs
+++ Assets/Scripts/UIScripts/PlayerUI.cs
@@ -10,6 +10,7 @@ public class PlayerUI : MonoBehaviour
     [SerializeField] TMP_Text waveTMP;
     private PlayerManager player;
     private GameManager gameManager;
+    private ShopMenu shopMenu;
     private int[] numOfShells;
 
     public void Init(PlayerManager playerRef, GameManager gameRef)
@@ -18,21 +19,38 @@ public class PlayerUI : MonoBehaviour
         gameManager = gameRef;
     }
 
+    private void Start()
+    {
+        shopMenu = GetComponentInChildren<ShopMenu>();
+    }
     void Update()
     {
-        if (player == null) return;
+        if (player == null || gameManager == null) { return; }
         int index = player.GetAmmoIndex();
         if (index >= 0 && index < ammoSprites.Length)
         {
             ammoIcon.sprite = ammoSprites[index];
         }
         numOfShells = player.GetAmmoCount();
-        ChangeAmmoCount(index);
+        if (numOfShells != null && index >= 0 && index < numOfShells.Length)
+        {
+            ChangeAmmoCount(index);
+        }
         waveTMP.SetText("Stage Lvl: " + gameManager.wavelvl.ToString());
+
+        if (Input.GetKeyDown(KeyCode.R)) //TODO MAKE SURE IT DOES NOT CONFLICT WITH PAUSE SCREEN
+        {
+            OpenShop();
+        }
     }
 
     public void ChangeAmmoCount(int index)
     {
         ammoCountTMP.SetText(numOfShells[index].ToString());
+    }
+
+    public void OpenShop() 
+    { 
+        shopMenu.isShopOpen = true;
     }
 }
--- Assets/Scripts/UIScripts/ShopMenu.cs
+++ Assets/Scripts/UIScripts/ShopMenu.cs
@@ -1,21 +1,16 @@
-using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ShopMenu : MonoBehaviour
 {
     public GameObject bgPanel;
-    public static bool isShopOpen = false;
+    [SerializeField] Sprite[] sprites = new Sprite[3];
     [SerializeField] float[] shellCosts = { 1.00f, 2.00f, 3.00f };
-    [SerializeField] Button up1B;
-    [SerializeField] Button up2B;
-    [SerializeField] Button up3B;
-    [SerializeField] Image up1I;
-    [SerializeField] Image up2I;
-    [SerializeField] Image up3I;
-    [SerializeField] TMP_Text up1T;
-    [SerializeField] TMP_Text up2T;
-    [SerializeField] TMP_Text up3T;
+    [SerializeField] Image uiImage;
+
+    public bool isShopOpen = false;
     public Color available = Color.white;
     public Color purchased = Color.gray;
     public Color lockedColor = new Color(0.35f, 0.35f, 0.35f, 1f);
@@ -23,81 +18,60 @@ public class ShopMenu : MonoBehaviour
 
     void Start()
     {
-
         bgPanel.SetActive(false);
-        RefreshUB();
+        uiImage.sprite = sprites[0];
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (isShopOpen)
         {
-            if (isShopOpen)
-                Resume();
-            else
-                Pause();
+            Pause();
         }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (!isShopOpen) { return; }
+            Resume();
+        }
+        ChangeBG();
     }
 
-    public void Up1()
+    public void ChangeBG() 
     {
-        if (ScoreManager.Instance != null)
+        if (lvl == 2)
         {
-            if (ScoreManager.Instance.money < shellCosts[0] || lvl != 0) return;
-            ScoreManager.Instance.SubtractMoney(shellCosts[0]);
-            lvl = 1;
-            RefreshUB();
+            uiImage.sprite = sprites[1];
+        }
+        else if (lvl == 3) 
+        { 
+            uiImage.sprite = sprites[2]; 
+        }
+
+        //Enough Money
+        if (ScoreManager.Instance.money > shellCosts[1] && lvl <= 2) 
+        {
+            uiImage.sprite = sprites[1];
+        }
+        if (ScoreManager.Instance.money > shellCosts[2] && lvl <= 3 && !(lvl <= 1))
+        {
+            uiImage.sprite = sprites[2];
         }
     }
 
-    public void Up2()
-    {
-        if (ScoreManager.Instance != null)
-        {
-            if (ScoreManager.Instance.money < shellCosts[1] || lvl != 1) return;
-            ScoreManager.Instance.SubtractMoney(shellCosts[1]);
-            lvl = 2;
-            RefreshUB();
-        }
+    public void BuyLvl1() 
+    { 
+        
     }
 
-    public void Up3()
-    {
-        if (ScoreManager.Instance != null)
-        {
-            if (ScoreManager.Instance.money < shellCosts[2] || lvl != 2) return;
-            ScoreManager.Instance.SubtractMoney(shellCosts[2]);
-            lvl = 3;
-            RefreshUB();
-        }
+    public void BuyLvl2() 
+    { 
+        
     }
 
-    void RefreshUB()
-    {
-        UBState(up1B, up1I, up1T, lvl == 0, lvl >= 1);
-        UBState(up2B, up2I, up2T, lvl == 1, lvl >= 2);
-        UBState(up3B, up3I, up3T, lvl == 2, lvl >= 3);
+    public void BuyLvl3() 
+    { 
+        
     }
-
-    void UBState(Button button, Image image, TMP_Text text, bool avail, bool bought)
-    {
-        button.interactable = avail;
-        if (avail)
-        {
-            image.color = available;
-            text.color = Color.black;
-            return;
-        }
-        if (bought)
-        {
-            image.color = purchased;
-            text.color = Color.white;
-            return;
-        }
-        image.color = lockedColor;
-        text.color = Color.gray;
-    }
-
 
     public void Resume()
     {
@@ -105,10 +79,83 @@ public class ShopMenu : MonoBehaviour
         Time.timeScale = 1f;
         isShopOpen = false;
     }
+
     public void Pause()
     {
         bgPanel.SetActive(true);
         Time.timeScale = 0f;
         isShopOpen = true;
     }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        isShopOpen = false;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+    /*
+        public void Up1()
+        {
+            if (ScoreManager.Instance != null)
+            {
+                if (ScoreManager.Instance.money < shellCosts[0] || lvl != 0) return;
+                ScoreManager.Instance.SubtractMoney(shellCosts[0]);
+                lvl = 1;
+                RefreshUB();
+            }
+        }
+
+        public void Up2()
+        {
+            if (ScoreManager.Instance != null)
+            {
+                if (ScoreManager.Instance.money < shellCosts[1] || lvl != 1) return;
+                ScoreManager.Instance.SubtractMoney(shellCosts[1]);
+                lvl = 2;
+                RefreshUB();
+            }
+        }
+
+        public void Up3()
+        {
+            if (ScoreManager.Instance != null)
+            {
+                if (ScoreManager.Instance.money < shellCosts[2] || lvl != 2) return;
+                ScoreManager.Instance.SubtractMoney(shellCosts[2]);
+                lvl = 3;
+                RefreshUB();
+            }
+        }
+
+        void RefreshUB()
+        {
+            UBState(up1B, up1I, up1T, lvl == 0, lvl >= 1);
+            UBState(up2B, up2I, up2T, lvl == 1, lvl >= 2);
+            UBState(up3B, up3I, up3T, lvl == 2, lvl >= 3);
+        }
+
+        void UBState(Button button, Image image, TMP_Text text, bool avail, bool bought)
+        {
+            button.interactable = avail;
+            if (avail)
+            {
+                image.color = available;
+                text.color = Color.black;
+                return;
+            }
+            if (bought)
+            {
+                image.color = purchased;
+                text.color = Color.white;
+                return;
+            }
+            image.color = lockedColor;
+            text.color = Color.gray;
+        }
+    */
+
+
+
 }
