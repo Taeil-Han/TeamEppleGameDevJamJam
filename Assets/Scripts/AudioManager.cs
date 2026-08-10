65a9ebd7b6669a97118a01e36f10ee1fda0aa6e8
Zelgius1070 <gentileaf7@gmail.com>
2026-08-09 20:34:02 -0400

    Audio Manager Updated

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -5,15 +5,37 @@ public class AudioManager : MonoBehaviour
     public static AudioManager Instance;
 
     [SerializeField] AudioSource sfxSource;
-    [SerializeField] AudioClip buttonClickSound;
+    [SerializeField] AudioClip UI_Confirm;
+    [SerializeField] AudioClip purchase;
+    [SerializeField] AudioClip UI_Cancel;
+    [SerializeField] AudioClip UI_Move;
 
     void Awake()
     {
         Instance = this;
     }
 
-    public void PlayButtonClick()
+    public void UIConfirmClick()
     {
-        sfxSource.PlayOneShot(buttonClickSound);
+        sfxSource.PlayOneShot(UI_Confirm);
     }
+
+    public void PlayPurchaseSound()
+    {
+
+        sfx.Source.PlayOneShot(purchase);
+
+    }
+
+    public void UICancelClick()
+    {
+        sfx.Source.PlayOneShot(UI_Cancel);
+    }
+
+    public void UiMovement()
+    {
+        sfx.Source.PlayOneShot(UI_Move);
+    }
+
+
 }
